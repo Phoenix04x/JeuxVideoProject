832f1f84702d50ae2e08a2c1f406cb3bc5ced938
chrisd <chrisd@localhost>
2022-07-26 11:50:40 +0200

    maj Newjeux,genre,plateforme

--- crudJeuxVideos/src/main/java/routing/JeuxServlet.java
+++ crudJeuxVideos/src/main/java/routing/JeuxServlet.java
@@ -27,6 +27,7 @@ public class JeuxServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.getServletContext().getRequestDispatcher("/WEB-INF/NewJeux.jsp").forward(request, response);
 	}
 
 	/**
